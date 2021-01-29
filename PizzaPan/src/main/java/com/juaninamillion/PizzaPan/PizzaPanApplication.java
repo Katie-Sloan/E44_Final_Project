3df8f38745f6029a7d64d7e8fc9bcf2bbfcf69aa
Maxwell Zilvester <zilimax@gmail.com>
2021-01-29 16:06:18 +0000

    Spring set up commit

---
+++ PizzaPan/src/main/java/com/juaninamillion/PizzaPan/PizzaPanApplication.java
@@ -0,0 +1,13 @@
+package com.juaninamillion.PizzaPan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaPanApplication.class, args);
+	}
+
+}
---
+++ PizzaPan/src/test/java/com/juaninamillion/PizzaPan/PizzaPanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaninamillion.PizzaPan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaPanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
