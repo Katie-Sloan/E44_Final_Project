7803e3c28403e6653357c3986e31172b76fcb8d0
lushpurr <lushpurr@gmail.com>
2021-01-31 16:23:27 +0000

    PizzaParser work

--- PizzaPan/src/main/java/com/juaninamillion/PizzaPan/jsonparsing/PizzaParser.java
+++ PizzaPan/src/main/java/com/juaninamillion/PizzaPan/jsonparsing/PizzaParser.java
@@ -14,17 +14,22 @@ public class PizzaParser {
 
     FoodTestingClass food;
 
+    public FoodTestingClass getFood() {
+        return food;
+    }
+
     private void startReading() {
         try {
             System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
-
-            food = mapper.readValue(new URL("https://api.spoonacular.com/food/menuItems/search?query=dominos%20pan%20pizza&apiKey=ff9b039211674a68a4864219f020d1ea&number=5"), FoodTestingClass.class);
+            food = mapper.readValue(new URL("https://api.spoonacular.com/food/menuItems/search?query=dominos%20pan%20pizza&apiKey=ff9b039211674a68a4864219f020d1ea&number=1"), FoodTestingClass.class);
 
             System.out.println(food);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 
 
--- PizzaPan/src/test/java/com/juaninamillion/PizzaPan/FoodTestingTest.java
+++ PizzaPan/src/test/java/com/juaninamillion/PizzaPan/FoodTestingTest.java
@@ -2,14 +2,18 @@ package com.juaninamillion.PizzaPan;
 
 
 import com.juaninamillion.PizzaPan.jsonparsing.PizzaParser;
+import com.juaninamillion.PizzaPan.models.Food;
+import com.juaninamillion.PizzaPan.models.FoodTestingClass;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FoodTestingTest {
     @Test
     public void loadJSON(){
         PizzaParser pizzaData = new PizzaParser();
+
+        System.out.println(pizzaData);
+        System.out.println(pizzaData.getFood().getRestaurantChain());
 
 
     }
