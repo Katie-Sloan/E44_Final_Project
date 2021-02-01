622f4105f95587858fa47ad9ee1aa845515584d7
Robert Hollingworth <robert@macbook-air.home>
2021-02-01 22:30:21 +0000

    Forgot to make a branch - added tables and parking spot fetch to main container

--- PizzaPan/src/main/java/com/juaninamillion/PizzaPan/controllers/OrderController.java
+++ PizzaPan/src/main/java/com/juaninamillion/PizzaPan/controllers/OrderController.java
@@ -1,13 +1,13 @@
 package com.juaninamillion.PizzaPan.controllers;
 
 import com.juaninamillion.PizzaPan.models.Order;
-import com.juaninamillion.PizzaPan.models.ParkingSpace;
+//import com.juaninamillion.PizzaPan.models.ParkingSpace;
 import com.juaninamillion.PizzaPan.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sun.security.provider.certpath.ResponderId;
+//import sun.security.provider.certpath.ResponderId;
 
 import java.util.List;
 
