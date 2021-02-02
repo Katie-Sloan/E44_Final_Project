ab27466ff9334e4172eff580df89d6087e2d0f45
lushpurr <lushpurr@gmail.com>
2021-02-02 11:55:59 +0000

    created route in main container and temporary nav bar for testing

---
+++ pizza_pan_client/src/components/header/TempNavbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TempNavBar = () => {
+    return (
+        <ul>
+            <li>
+                <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+                <Link to="/account">Account</Link>
+            </li>
+        </ul>
+    )
+
+}
+
+export default TempNavBar;
--- pizza_pan_client/src/containers/ContentContainer.js
+++ pizza_pan_client/src/containers/ContentContainer.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import MenuContainer from './MenuContainer';
 import AccountDetails from '../components/AccountDetails';
 
 const ContentContainer = () => {
-    
-    // logic goes here to determine what this is returning
-    if (true) {
-        return <MenuContainer />
-    }
+    return(
+        <Router>
+            <>
+            {/* <Route exact path="/" component={Home}/> */}
+            <Route path="/menu" component {...MenuContainer}/>
+            <Route path="/account" component {...AccountDetails}/>
+            </>
 
-    return (
-        <AccountDetails />
+        </Router>
     )
+
+
+
+
+
+
+
+    // // logic goes here to determine what this is returning
+    // if (true) {
+    //     return <MenuContainer />
+    // }
+
+    // return (
+    //     <AccountDetails />
+    // )
 }
 
 export default ContentContainer
