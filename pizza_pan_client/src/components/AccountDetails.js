c749ddcb7fa50277fcb8d71baecf874e2a969ebb
lushpurr <lushpurr@gmail.com>
2021-02-02 15:24:12 +0000

    added account form

--- pizza_pan_client/src/components/AccountDetails.js
+++ pizza_pan_client/src/components/AccountDetails.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import ".././style/account.css";
+
 
 const AccountDetails = () => {
     return (
-        <p>This is where the account form goes</p>
+        <>
+        <form>
+            <label for="name">Name:</label>
+            <input type="text" placeholder="Your name"/>
+            <label for="email">Email:</label>
+            <input type="text" placeholder="Your email"/>
+            <label for="password">Password</label>
+            <input type="password" placeholder="Your password"/>
+            <input type="submit" value="Submit"/>
+
+        </form>
+        </>
     )
 }
 
