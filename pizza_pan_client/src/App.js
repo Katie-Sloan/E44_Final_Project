5276b8dd7129de353342af6e7971cca51391a8a9
Maxwell Zilvester <zilimax@gmail.com>
2021-02-02 14:43:20 +0000

    resolve conflict

--- pizza_pan_client/src/App.js
+++ pizza_pan_client/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-import Profile from './components/Profile';
+
 import MainContainer from './containers/MainContainer';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppHeader from './components/header/AppHeader';
-import { Menu } from 'antd';
+
+
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       
       
       <MainContainer />
-      <Profile />
+      
       
     </>
   )
