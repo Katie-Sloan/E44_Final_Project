6aae5a56999a644d8ce70c4e1fc3ded3143b6b09
lushpurr <lushpurr@gmail.com>
2021-02-02 17:47:05 +0000

    further progress with accountdetail.js

--- pizza_pan_client/src/components/AccountDetails.js
+++ pizza_pan_client/src/components/AccountDetails.js
@@ -1,69 +1,86 @@
 import React, {useState} from 'react'
+import Request from '../helpers/request'
 import ".././style/account.css";
 
 
 const AccountDetails = () => {
+    const [users, setUsers] = useState([]);
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleNameChange = (evt) => {
-        setName(evt.target.value);
-    }
-
-    const handleEmailChange = (evt) => {
-        setEmail(evt.target.value);
-    }
-
-    const handlePasswordChange = (evt) => {
-        setPassword(evt.target.value);
-    }
-
-    const handleFormSubmit = (evt) => {
-        evt.preventDefault();
-        alert("testing");
-
-        const nameToSubmit = name.trim();
-        const emailToSubmit = email.trim();
-        const passwordToSubmit = password.trim();
-        if (!nameToSubmit || !emailToSubmit || !passwordToSubmit){
-            return
+    const [stateUser, setStateUser] = useState(
+        {
+            name: "",
+            email: "", 
+            password: ""
         }
-        setName("");
-        setEmail("");
-        setPassword("");
+    )
+
+    const requestAll = function(){
+        const request = new Request();
+        const userPromise = request.get('api/users')
+        .then((data) => {
+            setUsers(data);
+        })
     }
+
+    const handleChange = (evt) => {
+        let propertyName = evt.target.name;
+        let copiedUser = {...stateUser}
+        copiedUser[propertyName] = evt.target.value;
+        setStateUser(copiedUser)
+    }
+
+    // const handleEmailChange = (evt) => {
+    //     setEmail(evt.target.value);
+    // }
+
+    // const handlePasswordChange = (evt) => {
+    //     setPassword(evt.target.value);
+    // }
+    
+    // const handleFormSubmit = (evt) => {
+    //     evt.preventDefault();
+    //     alert("testing");
+
+    //     const nameToSubmit = name.trim();
+    //     const emailToSubmit = email.trim();
+    //     const passwordToSubmit = password.trim();
+    //     if (!nameToSubmit || !emailToSubmit || !passwordToSubmit){
+    //         return
+    //     }
+    //     setName("");
+    //     setEmail("");
+    //     setPassword("");
+    // }
 
     return (
         <>
         <h1>Update Account Details</h1>
-        <form className="account-details-form" onSubmit={handleFormSubmit}>
+        <form className="account-details-form" >
             <label for="name">Name:</label>
             <input  
                 type="text" 
                 placeholder="Your name"
-                value={name}
-                onChange={handleNameChange}
+                value={stateUser.name}
+                onChange={handleChange}
             />
 
             <label for="email">Email:</label>
             <input 
                 type="text" 
                 placeholder="Your email"
-                value={email}
-                onChange={handleEmailChange}
+                value={stateUser.email}
+                onChange={handleChange}
             />
 
             <label for="password">Password</label>
             <input 
                 type="password" 
                 placeholder="Your password"
-                value={password}
-                onChange={handlePasswordChange}
+                value={stateUser.password}
+                onChange={handleChange}
             />
 
-            <input type="submit" value="Submit"/>
+            <input type="submit" value="Save"/>
 
         </form>
         </>
