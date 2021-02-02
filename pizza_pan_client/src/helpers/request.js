aef68dfe32d68c093640279cda0530bcff227da3
lushpurr <lushpurr@gmail.com>
2021-02-02 20:53:36 +0000

    added post method to request.js

--- pizza_pan_client/src/helpers/request.js
+++ pizza_pan_client/src/helpers/request.js
@@ -6,6 +6,14 @@ class Request {
       .then((res) => res.json());
     }
 
+    post(url, payload){
+      return fetch(url, {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(payload)
+      })
+
+  } 
 }
 
 export default Request;
