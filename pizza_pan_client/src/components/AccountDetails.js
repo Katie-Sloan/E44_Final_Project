c5648bc7ce566f97d091cc749fb813f67f5c7b54
lushpurr <lushpurr@gmail.com>
2021-02-02 20:14:20 +0000

    saved account details befoere merge to feature/account

--- pizza_pan_client/src/components/AccountDetails.js
+++ pizza_pan_client/src/components/AccountDetails.js
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ".././style/account.css";
 
-const AccountDetails = () => {
+
+const AccountDetails = ({user, onCreate}) => {
+
+    // const [name, setName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+
+
+
+    const handleNameChange = (evt) => {
+        setName(evt.target.value);
+    }
+
+    const handleEmailChange = (evt) => {
+        setEmail(evt.target.value);
+    }
+
+    const handlePasswordChange = (evt) => {
+        setPassword(evt.target.value);
+    }
+    
+    const handleFormSubmit = (evt) => {
+        evt.preventDefault();
+        alert("testing");
+
+        const nameToSubmit = name.trim();
+        const emailToSubmit = email.trim();
+        const passwordToSubmit = password.trim();
+        if (!nameToSubmit || !emailToSubmit || !passwordToSubmit){
+            return
+        }
+        setName("");
+        setEmail("");
+        setPassword("");
+    }
+
     return (
-        <p>This is where the account form goes</p>
+        <>
+        <h1>Update Account Details</h1>
+        <form className="account-details-form" onSubmit={handleFormSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input  
+                type="text" 
+                placeholder="Your name"
+                value={name}
+                onChange={handleNameChange}
+            />
+
+            <label htmlFor="email">Email:</label>
+            <input 
+                type="text" 
+                placeholder="Your email"
+                value={email}
+                onChange={handleEmailChange}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input 
+                type="password" 
+                placeholder="Your password"
+                value={password}
+                onChange={handlePasswordChange}
+            />
+
+            <input type="submit" value="Submit"/>
+
+        </form>
+        </>
     )
 }
 
